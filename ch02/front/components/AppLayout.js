275cc313c520cf6ca16636b3e39aa52ebec54e2b
8c_6t <nkw8620@gmail.com>
2019-06-04 18:14:17 +0900

    2-4. 커스텀 훅 재사용하기

--- ch02/front/components/AppLayout.js
+++ ch02/front/components/AppLayout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
-import { Menu, Input, Button, Row, Col, Card, Avatar } from 'antd';
+import LoginForm from './LoginForm';
+import { Menu, Input, Button, Row, Col, Card, Avatar, Form } from 'antd';
 
 const dummy = {
   nickname: 'Hachicore',
   Post: [],
   Following: [],
   Followers: [],
+  isLoggedIn: false
 }
 
 const AppLayout = ({ children }) => {
@@ -26,18 +28,23 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row>
         <Col xs={24} md={6}>
-          <Card
-            actions={[
-              <div key="twit">트윗<br />{dummy.Post.length}</div>,
-              <div key="following">팔로잉<br />{dummy.Following.length}</div>,
-              <div key="follower">팔로워<br />{dummy.Followers.length}</div>,
-            ]}
-          >
-            <Card.Meta
-              avatar={ <Avatar>{ dummy.nickname[0] }</Avatar> }
-              title={ dummy.nickname }
-            />
-          </Card>
+          {dummy.isLoggedIn
+            ?
+            <Card
+              actions={[
+                <div key="twit">트윗<br />{dummy.Post.length}</div>,
+                <div key="following">팔로잉<br />{dummy.Following.length}</div>,
+                <div key="follower">팔로워<br />{dummy.Followers.length}</div>,
+              ]}
+            >
+              <Card.Meta
+                avatar={ <Avatar>{ dummy.nickname[0] }</Avatar> }
+                title={ dummy.nickname }
+              />
+            </Card>
+            : 
+            <LoginForm />
+          }
         </Col>
         <Col xs={24} md={12}>
           {children}
@@ -46,9 +53,6 @@ const AppLayout = ({ children }) => {
 
         </Col>
       </Row>
-      <Link href="/signup">
-        <a><Button>회원가입</Button></a>
-      </Link>
     </div>
   )
 }
--- ch02/front/pages/signup.js
+++ ch02/front/pages/signup.js
@@ -1,6 +1,15 @@
 import React, { useState, useCallback } from 'react';
 import { Form, Input, Checkbox, Button } from 'antd';
 
+// 커스텀 훅
+export const useInput = (initValue = null) => {
+  const [value, setter] = useState(initValue);
+  const handler = useCallback((e) => { 
+    setter(e.target.value);
+  }, []);
+  return [value, handler];
+}
+
 const Signup = () => {
 
   const [passwordCheck, setPasswordCheck] = useState('');
@@ -8,14 +17,6 @@ const Signup = () => {
   const [passwordError, setPasswordError] = useState(false);
   const [termError, setTermError] = useState(false);
 
-  // 커스텀 훅
-  const useInput = (initValue = null) => {
-    const [value, setter] = useState(initValue);
-    const handler = useCallback((e) => { 
-      setter(e.target.value);
-    }, []);
-    return [value, handler];
-  }
 
   const [id, onChangeId] = useInput('');
   const [nick, onChangeNick] = useInput('');
