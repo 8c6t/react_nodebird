1aea9bcc0976c1d7aae540227a94b4676ed768ad
8c_6t <nkw8620@gmail.com>
2019-06-05 16:01:35 +0900

    4-4. 사가의 제너레이터 이해하기

--- ch04/front/pages/index.js
+++ ch04/front/pages/index.js
@@ -10,6 +10,14 @@ const Home = () => {
   // 성능 최적화(리렌더링)를 위해 useSelector로 쪼개는 것이 좋다
   // const isLoggedIn = useSelector(state => state.user.isLoggedIn);
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'HELLO_SAGA' }),
+    dispatch({ type: 'HELLO_SAGA' }),
+    dispatch({ type: 'HELLO_SAGA' })
+  }, []);
+
   return (
     <div>
       { user ? <div>로그인 했습니다: { user.nickname } </div> : <div>로그아웃 했습니다.</div> }
--- ch04/front/sagas/user.js
+++ ch04/front/sagas/user.js
@@ -1,5 +1,7 @@
-import { all, fork, call, put, takeLatest } from 'redux-saga/effects';
+import { all, fork, call, put, take, takeLatest } from 'redux-saga/effects';
 import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from '../reducers/user';
+
+const HELLO_SAGA = "HELLO_SAGA";
 
 function loginAPI() {
   // 서버에 요청을 보내는 부분
@@ -25,8 +27,20 @@ function* watchLogin() {
   yield takeLatest(LOG_IN, login);
 }
 
+function* helloSaga() {
+  console.log('before saga!');
+  // 함수 자체가 종료되지 않도록,
+  // 같은 액션 dispatch에 대해 동일하게 응답할 수 있도록
+  // 무한 반복문을 사용
+  while(true) {
+    // take: 해당 액션이 dispatch 되면 제너레이터를 next 하는 이펙트
+    yield take(HELLO_SAGA);
+    console.log('hello saga!');
+    // 비동기 요청, 타이머 등...
+  }
+}
+
+// 사가 미들웨어에서 제너레이터의 next를 알아서 호출
 export default function* userSaga() {
-  yield all([
-    fork(watchLogin)
-  ]);
+  yield helloSaga();
 }
