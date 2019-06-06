213f35b9e87a7a86b37eba8f569c4ff0b6cabfee
8c_6t <nkw8620@gmail.com>
2019-06-06 20:05:36 +0900

    4-8. 사가 패턴과 QnA

--- ch04/front/reducers/user.js
+++ ch04/front/reducers/user.js
@@ -12,43 +12,62 @@ export const initialState = {
   signUpData: {},
 };
 
-export const LOG_IN = 'LOG_IN';
-export const LOG_OUT = 'LOG_OUT';
-export const SIGN_UP = 'SIGN_UP';
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
 export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
 
-export const loginAction = {
-  type: LOG_IN,
-  data: {
-    nickname: 'hachicore',
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
+
+export const signUpAction = (data) => ({
+  type: SIGN_UP_REQUEST,
+  data
+});
+
+export const singUpSuccess = {
+  type: SIGN_UP_SUCCESS
+}
+
+export const loginAction = (data) => {
+  return {
+    type: LOG_IN_REQUEST,
+    data
   }
 }
 
 export const logoutAction = {
-  type: LOG_OUT,
+  type: LOG_OUT_REQUEST,
 }
 
-export const signUpAction = (data) => ({
-  type: SIGN_UP,
-  data
-});
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
-    case LOG_IN:
+    case LOG_IN_REQUEST:
       return {
         ...state,
         isLoggedIn: true,
-        user: dummyUser
+        user: dummyUser,
+        loginData: action.data,
+        isLoading: true,
       }
-    case LOG_OUT:
+    case LOG_IN_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+      }
+    }
+    case LOG_OUT_REQUEST:
       return {
         ...state,
         isLoggedIn: false,
-        user: null
+        user: null,
       }
-    case SIGN_UP:
+    case SIGN_UP_REQUEST:
       return {
         ...state,
         signUpData: action.data
