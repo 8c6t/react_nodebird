e11787232f9bd5f9877d1f56d891568cd0c3c623
8c_6t <nkw8620@gmail.com>
2019-06-13 13:15:47 +0900

    5-9. 로그인을 위한 미들웨어들

--- ch05/back/index.js
+++ ch05/back/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const cookieParer = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const db = require('./models');
 
 const userAPIRouter = require('./routes/user');
@@ -15,6 +19,16 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParer(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false, // 매번 세션 강제 저장
+  saveUninitialized: false, // 빈 값도 저장
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true, // 자바스크립트에서 쿠키 조작 X
+    secure: false, // https
+  }
+}));
 
 app.use('/api/user', userAPIRouter);
 app.use('/api/post', postAPIRouter);
